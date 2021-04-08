90e7d9635ddc5b09c406cb80b97b6fe3a915f44c
MysickBoy <108527488+MysickBoy@users.noreply.github.com>
2021-04-08 09:02:10 +0000

    Version Update

--- linear_regression/least_square_sample_01.go
+++ linear_regression/least_square_sample_01.go
@@ -35,4 +35,4 @@ func makeBaseFunction(a_m int) func(float64) []float64 {
 func makePhiMatrix(a_vec []float64, a_baseFunction func(float64) []float64) (matrix [][]float64) {
 	matrix = make([][]float64, 0)
 	for _, x := range a_vec {
-		matrix = append(matrix, a_baseFunction(x)
+		matrix = append(matrix, a_baseFunction(x))
