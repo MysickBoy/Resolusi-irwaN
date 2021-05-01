0728ca6b39230c8ee6af82169429e09b448102b4
MysickBoy <108527488+MysickBoy@users.noreply.github.com>
2021-05-01 14:25:49 +0000

    Really commit all the required changes

--- linear_regression/least_square_sample_01.go
+++ linear_regression/least_square_sample_01.go
@@ -40,4 +40,4 @@ func makePhiMatrix(a_vec []float64, a_baseFunction func(float64) []float64) (mat
 	return
 }
 
-func f(a_w []float64, a_x float64, a_baseFunction func(float64) []f
+func f(a_w []float64, a_x float64, a_baseFunction func(float64) []floa
