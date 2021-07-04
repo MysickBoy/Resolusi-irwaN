f7efc079eb5e911e71e835680da2920951eb0bbc
MysickBoy <108527488+MysickBoy@users.noreply.github.com>
2021-07-04 17:17:09 +0000

    I18n: Update translation ko (100%).

--- linear_regression/least_square_sample_01.go
+++ linear_regression/least_square_sample_01.go
@@ -44,4 +44,4 @@ func f(a_w []float64, a_x float64, a_baseFunction func(float64) []float64) float
 	vecW := matrix.MakeDenseMatrix(a_w, 1, len(a_w))
 	phiX := a_baseFunction(a_x)
 	vecPhiX := matrix.MakeDenseMatrix(phiX, len(phiX), 1)
-	return matrix.Product(ve
+	return matrix.Product(vecW, v
