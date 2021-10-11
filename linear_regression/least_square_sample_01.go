3a2e4924d4003a9d36d693013120b52683af22e0
MysickBoy <108527488+MysickBoy@users.noreply.github.com>
2021-10-11 11:08:15 +0000

    Added javadoc

--- linear_regression/least_square_sample_01.go
+++ linear_regression/least_square_sample_01.go
@@ -47,4 +47,5 @@ func f(a_w []float64, a_x float64, a_baseFunction func(float64) []float64) float
 	return matrix.Product(vecW, vecPhiX).Get(0, 0)
 }
 
-func linspace(a_start, a_end float64, a_n int) (ret []float
+func linspace(a_start, a_end float64, a_n int) (ret []float64) {
+	re
