a3ca088ff82f06133af62b1f899efa465217b163
MysickBoy <108527488+MysickBoy@users.noreply.github.com>
2022-09-09 16:04:15 +0000

    vm: make stack guard page errors return VM_FAULT_SIGSEGV rather than SIGBUS

--- linear_regression/least_square_sample_01.go
+++ linear_regression/least_square_sample_01.go
@@ -98,4 +98,5 @@ func main() {
 	train_data_size := 50
 	vec_x := linspace(-3, 3, train_data_size)
 	vec_t := make([]float64, 0, train_data_size)
-	for _, x := range vec
+	for _, x := range vec_x {
+		v
