523dcfc45a37e0ed6e1e35b40ddb329a1f7a522c
MysickBoy <108527488+MysickBoy@users.noreply.github.com>
2023-02-04 22:56:30 +0000

    Build workflow

--- linear_regression/least_square_sample_01.go
+++ linear_regression/least_square_sample_01.go
@@ -123,4 +123,5 @@ func main() {
 	p.Y.Label.Text = "Y"
 	addLine(p, xlist, ylist)
 	addPoints(p, vec_x, vec_t)
-	p.Save(4, 4, "least_square_sample_01.png
+	p.Save(4, 4, "least_square_sample_01.png")
+}
